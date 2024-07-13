9b2a65374dfef3b5d3645ae7eeac413181414146
Flild <flildman@yandex.ru>
2024-07-13 18:44:47 +0300

    add platform service

--- Data/PlatformRepo.cs
+++ Data/PlatformRepo.cs
@@ -13,22 +13,27 @@ namespace PlatformService.Data
 
         public void CreatePlatform(Platform platform)
         {
-            throw new NotImplementedException();
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _context.Platforms.Add(platform);
         }
 
         public IEnumerable<Platform> GetAllPlatforms()
         {
-            throw new NotImplementedException();
+            return _context.Platforms.ToList();
         }
 
         public Platform GetPlatformById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
         }
 
         public bool SaveChanges()
         {
-            throw new NotImplementedException();
+            return (_context.SaveChanges() >= 0);
         }
     }
 }
